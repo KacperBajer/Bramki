25dcb4f0398b95dcb066f468515c8d7818f0b113
Kacper Bajer <kkacper4354@gmail.com>
2024-12-19 17:38:54 +0100

    fix

--- app/api/addCard/route.ts
+++ app/api/addCard/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
     try {
 
-        return  NextResponse.json({message: 'The feature is currently disabled'}, {status: 200})
+        return  NextResponse.json({message: 'The feature is currently disabled'}, {status: 500})
     } catch (error) {
         return  NextResponse.json({message: 'Something went wrong'}, {status: 500})
     }
