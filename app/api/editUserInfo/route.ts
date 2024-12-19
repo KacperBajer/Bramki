10ea81512e725b2df1ae55bbfc1a8c367d1cc9a7
Kacper Bajer <kkacper4354@gmail.com>
2024-12-19 17:05:02 +0100

    edit user info

--- app/api/editUserInfo/route.ts
+++ app/api/editUserInfo/route.ts
@@ -21,7 +21,7 @@ export async function PATCH(req: NextRequest) {
       return  NextResponse.json({message: 'Session expired'}, {status: 401})            
     }
 
-    console.log(user.id === parseInt(id))
+    console.log(id, user.id)
 
     if(user.id !== parseInt(id as string)) {
       console.log('d')
