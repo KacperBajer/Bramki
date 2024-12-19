d493e8905ca73b711c1aea02ef4a399af3420855
Kacper Bajer <kkacper4354@gmail.com>
2024-12-19 17:01:32 +0100

    edit user info

--- app/api/editUserInfo/route.ts
+++ app/api/editUserInfo/route.ts
@@ -18,16 +18,18 @@ export async function PATCH(req: NextRequest) {
     const user = await getUser(token)
     
     if(!user) {
-        return  NextResponse.json({message: 'Session expired'}, {status: 401})            
+      return  NextResponse.json({message: 'Session expired'}, {status: 401})            
     }
 
     if(user.id !== parseInt(id as string)) {
+      console.log('d')
       return  NextResponse.json({message: 'Something went wrong!'}, {status: 500})            
     }
 
     const edit = await editUser(parseInt(id as string), undefined, firstname, lastname, userClass)
 
     if(edit.status === 'failed') {
+      console.log('f')
       return NextResponse.json(
         { message: edit.error || "Something went wrong" },
         { status: 500 }
@@ -36,6 +38,7 @@ export async function PATCH(req: NextRequest) {
 
     return NextResponse.json({ message: "Success" }, { status: 200 });
   } catch (error) {
+    console.log(error)
     return NextResponse.json(
       { message: "Something went wrong" },
       { status: 500 }
